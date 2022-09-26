d2715656c0143742f87a297bc1f1d07b47204169
wiem-aboussaoud <114400104+wiem-aboussaoud@users.noreply.github.com>
2022-09-26 15:08:42 +0100

    Add files via upload

--- restaurent/settings.py
+++ restaurent/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     "rest_framework",
     'rest_framework.authtoken',  # add the auth token from the rest framework
+    'corsheaders',
 
     "rest_framework_swagger",
     "menu",
@@ -140,3 +141,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+CORS_ALLOW_ALL_ORIGINS = True
